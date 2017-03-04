515b6f22b91937017f9d483f5b771a756ad2dbca
Jindrich Skupa <jindrich.skupa@gmail.com>
2017-03-04 13:02:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ apt-get install curl openssh-server ca-certificates postfix
 # install gitlab
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
 apt-get install gitlab-ce
+gitlab-ctl reconfigure
+gitlab-ctl status
+
 
 # install gitlab runner
 curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | bash
 apt-get install gitlab-ci-multi-runner
+gitlab-ci-multi-runner register
